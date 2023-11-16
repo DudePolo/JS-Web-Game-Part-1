4996c6dfa43803a77918f6ff7e5d9346887953fd
DudePolo <andres.polo.wood@gmail.com>
2023-11-15 22:37:04 -0500

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(asset, dirLeft, dirBottom) {
+    let image = document.createElement('img')
+    image.src = asset
+    image.style.position = 'fixed'
+    image.style.left = dirLeft
+    image.style.bottom = dirBottom
+    document.body.append(image)
+}
+
+newImage('assets/green-character.gif', '100px', '100px')
+
+newImage('assets/pine-tree.png', '450px', '200px')
+
+newImage('assets/tree.png', '200px', '300px')
+
+newImage('assets/pillar.png', '350px', '100px')
+
+newImage('assets/crate.png', '150px', '200px')
+
+newImage('assets/well.png', '500px', '425px')
+
+function newItem(asset, dirLeft, dirBottom){
+    let item = document.createElement('img')
+    item.src = asset
+    item.position = 'fixed'
+    item.left = dirLeft
+    item.bottom = dirBottom
+    document.body.append(item)
+    
+    item.addEventListener('dblclick', function(){
+        item.remove()
+    }) 
+}
+
+newItem('assets/sword.png', '500px', '405px')
+
+newItem('assets/shield.png', '165px', '185px')
+
+newItem('assets/staff.png', '600px', '100px')
+
+
