8d059c712c1f0c5e88b61801649a5b4a0b385afb
DudePolo <andres.polo.wood@gmail.com>
2023-11-16 12:03:25 -0500

    Ok maybe a few more commits

--- index.js
+++ index.js
@@ -37,3 +37,37 @@ newItem('assets/sword.png', '500px', '405px')
 newItem('assets/shield.png', '165px', '185px')
 
 newItem('assets/staff.png', '600px', '100px')
+
+function backgroundImage(asset, left, bottom) {
+    let image = document.createElement('img')
+    image.src = asset
+    image.style.position = 'fixed'
+    image.style.zIndex = -1
+    image.style.left = left
+    image.style.bottom = bottom
+    document.body.append(image)
+}
+
+function createBackground(){
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let grassHeight = height *.52;
+    let skyHeight = height *.48;
+    
+
+    for(let i = 0; i < width; i+= 100){
+        for (let j = 0; j < grassHeight; j += 100){
+            backgroundImage('assets/grass.png', i + 'px', j + 'px')
+        }
+    }
+
+    for(let i = 0; i < width; i += 100){
+        for(let j = 0; j < skyHeight; j += 100){
+            let k = grassHeight + j;
+            backgroundImage('assets/sky.png', i + 'px', k + 'px');
+        }
+    }
+}
+
+
+createBackground();
